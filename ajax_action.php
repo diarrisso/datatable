0082e332ee008fb8f516b8ad8a7ef40a27eb4d2c
mamadidiarrisso <diarrisso49@gmail.com>
2022-09-25 00:19:35 +0200

    validation backend save

--- ajax_action.php
+++ ajax_action.php
@@ -31,32 +31,141 @@ if (empty($_POST['kundennummer'])) {
     {
         $modelCarrier = new Ontouch();
 
-        $row ['id'] =   $value['id'] ;
+        $row ['id'] =       $value['id'] ;
         $row ['kundennummer'] =   $value['kundennummer'] ;
-        $row ['name'] =   $value['name'] ;
-        $row ['urlSc'] =   $value['urlSc'] ;
+        $row ['name'] =           $value['name'] ;
+        $row ['urlSc'] =         $value['urlSc'] ;
         $row ['rufnummerSc'] =   $value['rufnummerSc'] ;
-        $row ['urlCc'] =   $value['urlCc'] ;
+        $row ['urlCc'] =         $value['urlCc'] ;
         $row ['rufnummerCc'] =   $value['rufnummerCc'] ;
         $row ['auftraggsart'] =   $value['auftraggsart'] ;
         $data [] = $row;
+
 
     }
     echo json_encode($data);
 }
 
+
+
 // function post insert;
+$db = new Database();
+$conn = $db->getConnection();
+//if (!empty($_POST['action']) && $_POST['action'] == 'addEmployee'){
+if ( !empty($_POST['kundennummer']) )
+{
 
-/*
-$error = array();
-if (!empty($_POST['kundennummer'])) {
 
-    $error = "das nist kein zahl";
+
+
+
+    $response = array();
+    $kundenummer  = $_POST['kundennummer'];
+
+//    if ( !filter_var($kundenummer , FILTER_VALIDATE_INT) )
+//    {
+//        $response = ['error'  => 'kundennummer muss en zahl sein '];
+//
+//        echo  json_encode($response);
+//        exit();
+//
+//    } else
+
+
+
+
+
+        $name         =  htmlspecialchars(trim($_POST['name']));
+        $urlSc         =  htmlspecialchars(trim($_POST['urlSc']));
+        $rufnummerSc         =  htmlspecialchars(trim($_POST['rufnummerSc']));
+        $urlSc         =  htmlspecialchars(trim($_POST['urlSc']));
+        $urlCc         =  htmlspecialchars(trim($_POST['urlCc']));
+        $rufnummerCc  = htmlspecialchars(trim($_POST['rufnummerCc']));
+        $auftraggsart = htmlspecialchars(trim($_POST['auftraggsart']));
+
+
+
+        //Sanitize input data using PHP filter_var().
+        $kundenummer =   filter_var($_POST["kundennummer"], FILTER_SANITIZE_NUMBER_INT);
+        $name      =     filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+        $urlSc     =     filter_var($_POST["urlSc"], FILTER_SANITIZE_STRING);
+        $rufnummerSc   = filter_var($_POST["rufnummerSc"], FILTER_SANITIZE_NUMBER_INT);
+        $rufnummerCc   = filter_var($_POST["rufnummerCc"], FILTER_SANITIZE_NUMBER_INT);
+        $urlCc        = filter_var($_POST["urlCc"], FILTER_SANITIZE_STRING);
+        $auftraggsart        = filter_var($_POST["auftraggsart"], FILTER_SANITIZE_STRING);
+
+        var_dump($auftraggsart);;
+        error_log('>>>>>>>'.$auftraggsart);
+        $output = array();
+
+        //additional php validation
+
+    if(!filter_var($kundenummer, FILTER_VALIDATE_INT)){ //email validation
+        $output = json_encode(array('type'=>'error', 'text' => 'kundennnumer!'));
+        die($output);
+    }
+
+
+
+    if(is_numeric($name) || strlen($name) < 3 )
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Name muss ein string sein!'));
+        die($output);
+    }
+
+
+
+    if(!filter_var($urlSc, FILTER_SANITIZE_STRING)){ //
+        $output = json_encode(array('type'=>'error', 'text' => 'urlsc link mit string'));
+        die($output);
+    }
+
+
+
+    if(!filter_var($rufnummerSc, FILTER_SANITIZE_NUMBER_FLOAT)){ //check for valid numbers in phone number field
+        $output = json_encode(array('type'=>'error', 'text' => 'rufnummerSc'));
+        die($output);
+    }
+
+
+
+    if(!filter_var($urlCc, FILTER_SANITIZE_STRING)){ //
+        $output = json_encode(array('type'=>'error', 'text' => 'urlcc'));
+        die($output);
+    }
+
+
+    if(!filter_var($rufnummerCc, FILTER_SANITIZE_NUMBER_FLOAT)){ //check for valid numbers in phone number field
+        $output = json_encode(array('type'=>'error', 'text' => 'rufnummerCc BIITE'));
+        die($output);
+    }
+
+
+
+
+    if(is_numeric($auftraggsart) || strlen($auftraggsart) < 3 ){ //
+        $output = json_encode(array('type'=>'error', 'text' => '$auftraggsart'));
+        die($output);
+    }
+
+
+
+
+        //echo json_encode($output);
+
+
+        $insertQuery = "INSERT INTO Blog.carrier(carrier.kundennummer, carrier.name, carrier.urlSc, carrier.rufnummerSc, carrier.urlCc, carrier.rufnummerCc, carrier.auftraggsart) 
+			VALUES ('".$kundenummer."', '".$name."', '".$urlSc."', '".$rufnummerSc."', '".$urlCc."','".$rufnummerCc."','".$auftraggsart."')";
+        $statement = $conn->prepare($insertQuery);
+        $statement->execute();
+
+
+
+
+
+
 
 }
-
-echo json_encode($error);*/
-
 
 
 
--- index.php
+++ index.php
@@ -18,90 +18,96 @@
 <div class="container">
 
 
-        <div class="panel-heading mt-4" style="float: right>
+    <div class="panel-heading mt-4" style="float: right>
             <div class="row">
 
-                <div class="col-md-2" ">
-<!--                    <button type="button" name="add" id="addEmployee" class="btn btn-success btn-xs">Add Employee</button>-->
-                    <button type="button" name="add" class="btn btn-primary" data-toggle="modal" data-target="#carrierModal" >Add Neue Eintrage</button>
+    <div class="col-md-2" ">
+    <!--                    <button type="button" name="add" id="addEmployee" class="btn btn-success btn-xs">Add Employee</button>-->
+    <button type="button" name="add" class="btn btn-primary" data-toggle="modal" data-target="#carrierModal" >Add Neue Eintrage</button>
+</div>
+</div>
+</div>
+<table id="onTouchCarrier" class="table table-bordered table-striped">
+    <thead>
+    <tr>
+        <th>id</th>
+        <th>kundennummer</th>
+        <th>name</th>
+        <th>urlsc</th>
+        <th>rufnummersc</th>
+        <th>urlcc</th>
+        <th>rufnummercc</th>
+        <th>auftragsart</th>
+        <th></th>
+        <th></th>
+    </tr>
+    </thead>
+</table>
+</div>
+
+<style>
+    show {
+        display: block;
+    }
+
+</style>
+
+<div id="carrierModal" class="modal fade">
+    <div class="modal-dialog">
+        <form method="post" id="carrierForm">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title"><i class="fa fa-plus"></i> Edit User</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group"
+                    <label for="kundennummer" class="control-label">kundennummer</label>
+                    <input type="text" class="form-control" id="kundennummer" name="kundennummer" placeholder="kundennummer" required>
+                    <span class="error" id="kundennummer_erro"> </span>
+                </div>
+                <div class="form-group">
+                    <label for="name" class="control-label">name</label>
+                    <input type="text" class="form-control" id="name" name="name" placeholder="name" required>
+                    <span class="error" id="name_erro"> </span>
+                </div>
+                <div class="form-group">
+                    <label for="urlSc" class="control-label">urlSc</label>
+                    <input type="url" class="form-control"  id="urlSc" name="urlSc" placeholder="urlSc" >
+                </div>
+                <div class="form-group">
+                    <label for="rufnummerSc" class="control-label">rufnummerSc</label>
+                    <input type="tel" class="form-control"  id="rufnummerSc" name="rufnummerSc">
+                </div>
+                <div class="form-group">
+                    <label for="urlCc" class="control-label">urlCc</label>
+                    <input type="url" class="form-control" id="urlCc" name="urlCc" placeholder="urlCc">
+                </div>
+                <div class="form-group">
+                    <label for="$rufnummerCc" class="control-label">rufnummerCc</label>
+                    <input type="tel" class="form-control" id="rufnummerCc" name="rufnummerCc" placeholder="rufnummerCc">
+                </div>
+
+                <div class="form-group">
+                    <label for="auftraggsart" class="control-label">auftragsart</label>
+                    <input type="text" class="form-control" id="auftraggsart" name="auftraggsart" placeholder="auftragsart">
+                    <span class="error" id="auftraggsart_error"> </span>
                 </div>
             </div>
-        </div>
-        <table id="onTouchCarrier" class="table table-bordered table-striped">
-            <thead>
-            <tr>
-                <th>id</th>
-                <th>kundennummer</th>
-                <th>name</th>
-                <th>urlsc</th>
-                <th>rufnummersc</th>
-                <th>urlcc</th>
-                <th>rufnummercc</th>
-                <th>auftragsart</th>
-                <th></th>
-                <th></th>
-            </tr>
-            </thead>
-        </table>
+            <div class="modal-footer">
+                <input type="hidden" name="id" id="id" />
+                <input type="hidden" name="action" id="action" value="" />
+                <input type="submit" name="save" id="save" class="btn btn-info" value="Save" />
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
     </div>
-
-    <div id="carrierModal" class="modal fade">
-        <div class="modal-dialog">
-            <form method="post" id="carrierForm">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title"><i class="fa fa-plus"></i> Edit User</h4>
-                    </div>
-                    <div class="modal-body">
-                        <div class="form-group"
-                        <label for="kundennummer" class="control-label">kundennummer</label>
-                        <input type="text" class="form-control" id="kundennummer" name="kundennummer" placeholder="kundennummer" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="name" class="control-label">name</label>
-                        <input type="text" class="form-control" id="name" name="name" placeholder="name" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="urlSc" class="control-label">urlSc</label>
-                        <input type="url" class="form-control"  id="urlSc" name="urlSc" placeholder="urlSc" >
-                    </div>
-                    <div class="form-group">
-                        <label for="rufnummerSc" class="control-label">rufnummerSc</label>
-                        <input type="tel" class="form-control"  id="rufnummerSc" name="rufnummerSc">
-                    </div>
-                    <div class="form-group">
-                        <label for="urlCc" class="control-label">urlCc</label>
-                        <input type="url" class="form-control" id="urlCc" name="urlCc" placeholder="urlCc">
-                    </div>
-                    <div class="form-group">
-                        <label for="$rufnummerCc" class="control-label">rufnummerCc</label>
-                        <input type="tel" class="form-control" id="rufnummerCc" name="rufnummerCc" placeholder="rufnummerCc">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="auftragsart" class="control-label">auftragsart</label>
-                        <input type="text" class="form-control" id="auftragsart" name="auftragsart" placeholder="auftragsart">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <input type="hidden" name="id" id="id" />
-                    <input type="hidden" name="action" id="action" value="" />
-                    <input type="submit" name="save" id="save" class="btn btn-info" value="Save" />
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
-        </div>
-        </form>
-    </div>
+    </form>
+</div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" ></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-
-
-
-
 
 
 <script>
@@ -138,37 +144,128 @@
 
         $("#carrierModal").on('submit', '#carrierForm', function (event) {
             event.preventDefault();
-            console.log('salut');
+            $('#kundennummer').on('input', function() {
+                checkckundennummer();
+            });
+            $('#name').on('input', function() {
+                checkcname();
+            });
+            $('#auftraggsart').on('input', function() {
+                checkauftragsart();
+            });
+
+            if (!checkckundennummer() && !checkcname() && !checkauftragsart() ) {
+
+                $("#message").html('<div class="alert alert-warning">Please fill all required field</div>');
+            } else if (!checkckundennummer() || !checkcname() || !checkauftragsart()) {
+                $("#message").html('<div class="alert alert-warning">Please fill all required field</div>');
+
+            }
+
             $('#save').attr('disabled', 'disabled');
             var formData = $(this).serialize();
+
+
             $.ajax({
                 url: "ajax_action.php",
                 method: "POST",
                 data: formData,
+
+                // beforeSend: function() {
+                //     $('#save').html('<i class="fa-solid fa-spinner fa-spin"></i>');
+                //     $('#save').attr("disabled", true);
+                //     $('#save').css({
+                //         "border-radius": "50%"
+                //     });
+                // },
                 success: function (data) {
-                    $('#carrierForm')[0].reset();
-                    $('#carrierModal').modal('hide');
-                    $('#save').attr('disabled', false);
+                    if(data.type === 'error'){ //load json data from server and output message
+                        output = '<div class="error">'+data.text+'</div>';
+
+                        console.log('entrere nous');
+                    }else{
+                        output = '<div class="success">'+data.text+'</div>';
+                        //reset values in all input fields
+                        $("#contact_form  input[required=true], #contact_form textarea[required=true]").val('');
+                        $("#contact_form #contact_body").slideUp(); //hide form after success
+                    }
+
+                    // $('#carrierForm')[0].reset();
+                    // $('#carrierModal').modal('hide');
+                    // $('#save').attr('disabled', false);
                     //dataRecords.ajax.reload();
                 }
-            })
+            });
+
         });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     } );
+
+
+
+    function checkckundennummer() {
+
+        var kundennumer = $('#kundennummer').val();
+
+        if ( kundennumer === '') {
+            $('#kundennummer_erro').html('kundennunner Field is required');
+            return false;
+        } else if ($('#kundennummer').val().length < 4) {
+            $('#kundennummer_erro').html('kundenummer length is too short');
+            return false;
+        } else {
+            $('#kundennummer_erro').html('');
+            return true;
+        }
+    }
+
+
+    function checkcname() {
+
+        var name = $('#name').val();
+
+        if ( name === '') {
+            $('#name_erro').html('kundennunner Field is required');
+            return false;
+        } else if ($('#name').val().length < 4) {
+            $('#name_erro').html('kundenummer length is too short');
+            return false;
+        } else {
+            $('#name_erro').html('');
+            return true;
+        }
+    }
+
+
+    function checkauftragsart() {
+
+        var pattern = /^[A-Za-z0-9]+$/;
+        var auftraggsart = $('#auftraggsart').val();
+
+
+        if (auftraggsart === '') {
+            $('#auftraggsart_error').html('Auftragsart Field is required');
+            return false;
+        } else if ($('#auftraggsart').val().length < 4) {
+            $('#auftraggsart_error').html('auftragsart length is too short');
+            return false;
+        }
+        else {
+            $('#auftraggsart_error').html('');
+            return true;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
